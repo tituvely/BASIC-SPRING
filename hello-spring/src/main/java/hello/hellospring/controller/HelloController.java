f4257c300e60f3739d5fdd9153c9d5e53a06faff
Kim Yoo Kyung <tituvely@gmail.com>
2021-08-21 21:27:53 +0900

    컴포넌트 스캔

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public HelloController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @GetMapping("hello")
     public String hello(Model model) {
