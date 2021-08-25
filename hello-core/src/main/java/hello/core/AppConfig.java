772ee8755234a9e7dd89cc5137cbefea0394db23
Yookyung Kim <tituvely@gmail.com>
2021-08-25 17:44:19 +0900

    @Configuration을 적용하지 않고, @Bean만 적용하는 경우

--- hello-core/src/main/java/hello/core/AppConfig.java
+++ hello-core/src/main/java/hello/core/AppConfig.java
@@ -12,7 +12,10 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration 삭제
+//@Bean만 사용해도 스프링 빈으로 등록되지만, 싱글톤을 보장하지 않는다.
+//memberRepository() 처럼 의존관계 주입이 필요해서 메서드를 직접 호출할 때 싱글톤을 보장하지 않는다.
+//크게 고민할 것이 없다. 스프링 설정 정보는 항상 @Configuration 을 사용하자.
 public class AppConfig {
 
     @Bean
