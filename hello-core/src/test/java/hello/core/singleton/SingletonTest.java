cbc6106cde22a78ac9685ea06910fedb0c3d5bcd
Yookyung Kim <tituvely@gmail.com>
2021-08-25 17:09:43 +0900

    스프링 컨테이너(싱글톤 컨테이너)를 사용하여 싱글톤 적용

--- hello-core/src/test/java/hello/core/singleton/SingletonTest.java
+++ hello-core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -3,9 +3,12 @@ package hello.core.singleton;
 import hello.core.AppConfig;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -55,4 +58,25 @@ public class SingletonTest {
 
         singletonService1.logic();
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }
