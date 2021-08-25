4bc796d7ffd5cba9fe2b8c58daa16003117c6766
Kim Yoo Kyung <tituvely@gmail.com>
2021-08-25 22:36:24 +0900

    빈 중복 등록과 충돌 - 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생한다

--- hello-core/src/main/java/hello/core/CoreApplication.java
+++ hello-core/src/main/java/hello/core/CoreApplication.java
@@ -10,4 +10,10 @@ public class CoreApplication {
         SpringApplication.run(CoreApplication.class, args);
     }
 
+    // 하지만, 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+    // The bean 'memoryMemberRepository', defined in class path resource could not be registered.
+    // A bean with that name has already been defined in file and overriding is disabled.
+
+    // application.properties에서 overriding을 true로 해주면 동작한다.
+    // 기본값은 false
 }
