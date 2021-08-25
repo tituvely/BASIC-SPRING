17e316049b7aa95fbd281e23e0002c74050a7936
Yookyung Kim <tituvely@gmail.com>
2021-08-25 17:02:54 +0900

    싱글톤 패턴을 적용한 객체 사용

---
+++ hello-core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,22 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    //1. static 영역에 객체 인스턴스를 미리 1개 생성해둔다.
+    private static final SingletonService instance = new SingletonService();
+
+    //2. 객체 인스턴스가 필요하면 오직 public static인 getInstance() 메서드를 통해서만 조회할 수 있다.
+    // 이 메서드를 호출하면 항상 같은 인스턴스를 반환한다.
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    //3. 생성자를 private으로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 막는다.
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+}
--- hello-core/src/test/java/hello/core/singleton/SingletonTest.java
+++ hello-core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -31,6 +31,28 @@ public class SingletonTest {
 
 //    우리가 만들었던 스프링 없는 순수한 DI 컨테이너인 AppConfig는 요청을 할 때 마다 객체를 새로 생성한다.
 //    고객 트래픽이 초당 100이 나오면 초당 100개 객체가 생성되고 소멸된다! 메모리 낭비가 심하다.
-//    해결방안은 해당 객체가 딱 1개만 생성되고, 공유하도록 설계하면 된다. 싱글톤 패턴
+//    해결방안은 해당 객체가 딱 1개만 생성되고, 공유하도록 설계하면 된다. -> 싱글톤 패턴
 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
+        //new SingletonService();
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        //isSameAs: 참조값이 같은지 확인
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.logic();
+    }
 }
