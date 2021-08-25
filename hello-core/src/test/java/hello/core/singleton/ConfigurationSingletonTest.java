1ab1845e6218f739c832a7747323d8361a0c196f
Yookyung Kim <tituvely@gmail.com>
2021-08-25 17:38:42 +0900

    @Configuration과 CGLIB 라이브러리(바이트 코드 조작)

--- hello-core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ hello-core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,25 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        //AnnotationConfigApplicationContext에 파라미터로 넘긴 값은 스프링 빈으로 등록된다
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$b390c159
+
+//        순수한 클래스라면 다음과 같이 출력되어야 한다.
+//        class hello.core.AppConfig
+//        즉, 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서
+//        AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것
+
+//        @Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고,
+//        스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다.
+//        덕분에 싱글톤이 보장되는 것이다.
+    }
 }
