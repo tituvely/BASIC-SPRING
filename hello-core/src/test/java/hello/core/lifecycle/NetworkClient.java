70faac55ccf834fc1248c93a7dbd07e268b00f36
Yookyung Kim <tituvely@gmail.com>
2021-08-26 13:55:05 +0900

    빈 LifeCycle 콜백 지원 - 애노테이션(@PostConstruct, @PreDestroy)

--- hello-core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ hello-core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,10 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +32,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() {
+    @PostConstruct
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() {
+    @PreDestroy
+    public void close() {
         disconnect();
     }
 
