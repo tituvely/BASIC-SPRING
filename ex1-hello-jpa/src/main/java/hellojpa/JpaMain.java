591757b7379cf33da46ab25efcf2afb21f1dc280
Yookyung Kim <tituvely@gmail.com>
2021-08-30 12:41:19 +0900

    객체와 테이블 생성 및 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,18 +2,44 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
+        // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 엔티티 매니저는 쓰레드간에 공유X (사용하고 버려야 한다).
+        EntityManager em = emf.createEntityManager();
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction tx = em.getTransaction();
 
-        EntityManager entityManager = emf.createEntityManager();
+        tx.begin();
 
-        entityManager.close();
+        try {
+            Member member = new Member();
+            member.setName("memberA");
+            member.setId(1L);
+
+            // 회원 등록
+            em.persist(member);
+
+            // 회원 조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // 회원 수정
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close();
-
     }
 }
