d41668d6134710f1eb0080e760ae72c4ca5c23d9
Kim Yoo Kyung <tituvely@gmail.com>
2021-09-03 23:20:27 +0900

    다대대 관계 - 권장x

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -20,6 +22,10 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @ManyToMany
+    @JoinTable(name = "MEMBER_PRODUCT")
+    List<Product> products = new ArrayList<>();
 
     public Long getId() {
         return id;
