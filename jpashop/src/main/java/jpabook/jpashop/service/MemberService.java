baab9c5d05157f50eb31bd8b22b7db0b8bfa7cee
Kim Yoo Kyung <tituvely@gmail.com>
2021-09-05 20:32:20 +0900

    회원 서비스 개발 - 2 (필드 주입 -> 생성자 주입)

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -12,8 +12,13 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    // final 키워드를 추가하면 컴파일 시점에 memberRepository를 설정하지 않는 오류를 체크할 수 있다.
+    private final MemberRepository memberRepository;
+
+    @Autowired //생성자가 하나면, @Autowired 를 생략할 수 있다.
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
