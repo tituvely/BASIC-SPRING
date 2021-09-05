d9b1800a238b693122e35796bee9b70779cb7386
Kim Yoo Kyung <tituvely@gmail.com>
2021-09-05 20:33:26 +0900

    회원 서비스 개발 - 3 (생성자 주입시 lombok의 @AllArgsConstructor 사용)

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,15 +11,11 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@AllArgsConstructor
 public class MemberService {
 
     // final 키워드를 추가하면 컴파일 시점에 memberRepository를 설정하지 않는 오류를 체크할 수 있다.
     private final MemberRepository memberRepository;
-
-    @Autowired //생성자가 하나면, @Autowired 를 생략할 수 있다.
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     /**
      * 회원가입
