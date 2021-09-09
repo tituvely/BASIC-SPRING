8a131df1173e4232b62c810d5ca2a4546fba1cf4
Yookyung Kim <tituvely@gmail.com>
2021-09-09 23:26:27 +0900

    상품 수정 권장 코드 - 변경 감지 사용

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -21,16 +21,14 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-
+    // 가장 좋은 해결 방법: 엔티티를 변경할 때는 항상 변경 감지를 사용하세요
     // 영속성 컨텍스트에서 엔티티를 다시 조회한 후에 데이터를 수정하는 방법
     @Transactional
-    public void updateItem(Long ItemId, Book param) {
-        //트랜잭션 안에서 엔티티를 다시 조회, 변경할 값 선택
-        //트랜잭션 커밋 시점에 변경 감지(Dirty Checking)이 동작해서 데이터베이스에 UPDATE SQL 실행
-        Item findItem = itemRepository.findOne(ItemId);
-        findItem.setPrice(param.getPrice());
+    public void updateItem(Long id, String name, int price) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
     }
-
 
     public List<Item> findItems() {
         return itemRepository.findAll();
