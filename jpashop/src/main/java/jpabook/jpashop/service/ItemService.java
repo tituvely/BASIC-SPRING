1f0f478da85edc2497902057c27fa1b3dd6e24ff
Yookyung Kim <tituvely@gmail.com>
2021-09-09 23:15:39 +0900

    준영속 엔티티를 수정하는 방법 - 변경감지(더티체킹)

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,17 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
+
+    // 영속성 컨텍스트에서 엔티티를 다시 조회한 후에 데이터를 수정하는 방법
+    @Transactional
+    public void updateItem(Long ItemId, Book param) {
+        //트랜잭션 안에서 엔티티를 다시 조회, 변경할 값 선택
+        //트랜잭션 커밋 시점에 변경 감지(Dirty Checking)이 동작해서 데이터베이스에 UPDATE SQL 실행
+        Item findItem = itemRepository.findOne(ItemId);
+        findItem.setPrice(param.getPrice());
+    }
+
 
     public List<Item> findItems() {
         return itemRepository.findAll();
