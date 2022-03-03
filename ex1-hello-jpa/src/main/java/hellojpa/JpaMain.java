23a6cda5cb95fcf0404feec2568debee5bf6ac45
Yookyung Kim <tituvely@gmail.com>
2022-03-03 16:48:34 +0900

    JPQL - 객체지향 SQL

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,44 +20,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("hello");
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
 
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "zipcode"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "zipcode"));
-
-            em.persist(member);
-
-//            em.flush();
-//            em.clear();
-//
-//            System.out.println("========START========");
-//            Member findMember = em.find(Member.class, member.getId());
-//
-//            List<Address> addressHistory = findMember.getAddressHistory();
-//            for(Address address: addressHistory) {
-//                System.out.println("address = " + address.getCity());
-//            }
-//
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-//            for(String favoriteFood: favoriteFoods) {
-//                System.out.println("favoriteFood = " + favoriteFood);
-//            }
-//
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("new city", a.getStreet(), a.getZipcode()));
-//
-//            System.out.println("========FAVORITE FOODS========");
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            System.out.println("========ADDRESS========");
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "zipcode"));
-//            findMember.getAddressHistory().add(new Address("new city1", "street", "zipcode"));
+            List<Member> result = em.createQuery(
+                    "select m From Member m where m.name like '%kim%'",
+                        Member.class)
+                .getResultList();
 
             tx.commit();
         } catch (Exception e) {
