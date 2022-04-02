1a5b423109243190a67564502bdf9d43d7f7ba1e
Yookyung Kim <tituvely@gmail.com>
2022-04-02 19:35:54 +0900

    프로퍼티 사용하는 방법 @PropertySource JVM option @Value
    프로퍼티에는 우선순위가 있음

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
@@ -2,8 +2,10 @@ package com.example.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class SpringapplicationcontextApplication {
 
     public static void main(String[] args) {
