25db517e1c6ecd46ad38605c2b814b96670b9c38
Yookyung Kim <tituvely@gmail.com>
2022-04-02 20:12:31 +0900

    비동기적으로 실행하고 싶으면 @Async 사용

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/AnotherHandler.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/AnotherHandler.java
@@ -1,15 +1,14 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Async
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread());
         System.out.println("Another " + myEvent.getData());
--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/MyEventHandler.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/MyEventHandler.java
@@ -1,15 +1,14 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
+    @Async
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread());
         System.out.println("이벤트를 받았다. 데이터는 " + event.getData());
