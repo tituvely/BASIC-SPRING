cd7c87910ee04ecb6bdc94eb550c6c30397d4a00
Yookyung Kim <tituvely@gmail.com>
2022-04-03 02:10:07 +0900

    스프링 부트 - Webapp적용 x

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
@@ -1,13 +1,16 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringapplicationcontextApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringapplicationcontextApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringapplicationcontextApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
 }
