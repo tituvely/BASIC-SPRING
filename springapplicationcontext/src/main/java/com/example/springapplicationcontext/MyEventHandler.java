655d1e7dd3763f17bcb1686c35452fa0abd74ce3
Yookyung Kim <tituvely@gmail.com>
2022-04-02 20:15:27 +0900

    스프링이 제공하는 기본 이벤트

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/MyEventHandler.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/MyEventHandler.java
@@ -1,5 +1,7 @@
 package com.example.springapplicationcontext;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,21 @@ public class MyEventHandler {
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread());
         System.out.println("이벤트를 받았다. 데이터는 " + event.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread());
+        System.out.println("ApplicationContext를 초기화하거나 리프레시할때");
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread());
+        System.out.println("ApplicationContext를 close할 때");
+        System.out.println("ContextClosedEvent");
     }
 }
