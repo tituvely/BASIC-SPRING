09ba7c1870832da05cef34a70e4fffce86aa34de
Yookyung Kim <tituvely@gmail.com>
2022-04-02 18:16:11 +0900

    펑션을 사용해 빈을 등록 -> 애플리케이션 구동 시 성능상의 이점이 있음(프록시를 쓰지 않기 때문)
    - 하지만, ComponentScan을 포기하고 펑셔널하게 빈을 등록하기에는 너무 복잡할것
    - ComponenScan 범위 밖에 있는 MyService같은 빈들만 펑셔널 방법을 사용해 등록하는 것이 바람직

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/SpringapplicationcontextApplication.java
@@ -2,10 +2,14 @@ package com.example.springapplicationcontext;
 
 import com.example.out.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SpringapplicationcontextApplication {
@@ -15,11 +19,19 @@ public class SpringapplicationcontextApplication {
 
     public static void main(String[] args) {
         var app = new SpringApplication(SpringapplicationcontextApplication.class);
-        app.addInitializers(new ApplicationContextInitializer<GenericApplicationContext>() {
-            @Override
-            public void initialize(GenericApplicationContext ctx) {
-                ctx.registerBean(MyService.class);
-            }
+        app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) ctx -> {
+            ctx.registerBean(MyService.class);
+            ctx.registerBean(ApplicationRunner.class, new Supplier<ApplicationRunner>() {
+                @Override
+                public ApplicationRunner get() {
+                    return new ApplicationRunner() {
+                        @Override
+                        public void run(ApplicationArguments args) throws Exception {
+                            System.out.println("Function Bean Definition");
+                        }
+                    };
+                }
+            });
         });
         app.run(args);
     }
