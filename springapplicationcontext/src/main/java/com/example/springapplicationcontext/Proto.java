59b9a0e3e83e9a8f8782f233ae55bbfc7387de1f
Yookyung Kim <tituvely@gmail.com>
2022-04-02 18:34:30 +0900

    proxy 모드를 사용해 Proto를 proxy로 감싸, 싱글턴 내에서 Proto빈을 필드로 가질 경우 Proto빈을 매번 생성하도록 함

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/Proto.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/Proto.java
@@ -1,10 +1,11 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Proto {
 
 }
