8db214894a5614b4089dbc16992ff31a87fce3a9
Yookyung Kim <tituvely@gmail.com>
2022-04-03 02:27:40 +0900

    스프링 AOP 적용 - @Before

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/PerfAspect.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/PerfAspect.java
@@ -3,6 +3,7 @@ package com.example.springapplicationcontext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,5 +16,10 @@ public class PerfAspect {
         Object retVal = pjp.proceed();
         System.out.println(System.currentTimeMillis() - begin);
         return retVal;
+    }
+
+    @Before("bean(simpleEventService)")
+    public void hello() {
+        System.out.println("hello");
     }
 }
