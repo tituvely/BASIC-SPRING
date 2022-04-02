d050779e3cdbf347f3f6a1d8275b49071a523834
Yookyung Kim <tituvely@gmail.com>
2022-04-02 23:27:50 +0900

    Validator와 ValidationUtil을 사용해 검증

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
@@ -1,26 +1,28 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ApplicationContext resourceLoader;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
+        Event event = new Event();
+        EventValidator eventValidator = new EventValidator();
+        Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        Resource resource = resourceLoader.getResource("test.txt");
-        System.out.println(resource.getClass());
+        eventValidator.validate(event, errors);
 
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("=============== error code ==============");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
---
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/Event.java
@@ -0,0 +1,24 @@
+package com.example.springapplicationcontext;
+
+public class Event {
+
+    Integer id;
+
+    String title;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
