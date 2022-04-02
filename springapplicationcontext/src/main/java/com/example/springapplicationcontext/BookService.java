d7469930afe411221fd83c71fcdd33bbd1f305da
Yookyung Kim <tituvely@gmail.com>
2022-04-02 17:00:16 +0900

    Spring 빈 설정 방법 - JAVA로 하는 빈 설정 Configuration

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
@@ -1,12 +1,7 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class BookService {
 
-    @Autowired
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
