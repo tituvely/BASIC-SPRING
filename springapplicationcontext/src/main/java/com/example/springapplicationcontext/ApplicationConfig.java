bfce7e7d800ec0e78a7d1f94ef5274922b34c59a
Yookyung Kim <tituvely@gmail.com>
2022-04-02 17:11:12 +0900

    Spring 빈 설정 방법 - @SpringBootApplication 이 이 모든걸 다 해줌

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapplicationcontext;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = SpringapplicationcontextApplication.class)
-public class ApplicationConfig {
-}
