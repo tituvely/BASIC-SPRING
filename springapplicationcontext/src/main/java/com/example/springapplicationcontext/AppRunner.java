baeeb19cde49740ccbf6b41943d3e940d1e70431
Yookyung Kim <tituvely@gmail.com>
2022-04-02 22:14:56 +0900

    Resource의 타입은 ApplicationContext의 타입에 의해 결정된다. ApplicationContext와 상관없이 리소스 타입을 강제하려면 file:///나 classpath:같은 접두어를 붙여준다.
    - ClassPathXmlApplicationContext -> ClassPathResource
    - FileSystemXmlApplicationContext -> FileSystemResource
    - WebApplicationContext -> ServletContextResource

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
@@ -3,26 +3,24 @@ package com.example.springapplicationcontext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:/test.txt");
+        System.out.println(resourceLoader.getClass());
+
+        Resource resource = resourceLoader.getResource("test.txt");
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
-        System.out.println(resource.getURI());
-        System.out.println(Path.of(resource.getURI()));
-        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
