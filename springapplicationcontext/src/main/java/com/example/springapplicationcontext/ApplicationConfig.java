6ae4fbf5008c96df7c7f5272316dabcd90fb7f2a
Yookyung Kim <tituvely@gmail.com>
2022-04-02 17:07:45 +0900

    Spring 빈 설정 방법 - JAVA 기반 Configuration + Annotation 기반의 ComponentScan

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/ApplicationConfig.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/ApplicationConfig.java
@@ -1,18 +1,9 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = SpringapplicationcontextApplication.class)
 public class ApplicationConfig {
-
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService() {
-        return new BookService();
-    }
 }
--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
@@ -1,7 +1,9 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
     @Autowired
