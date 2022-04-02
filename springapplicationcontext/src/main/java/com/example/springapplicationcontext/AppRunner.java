a625210709b4430201d5130a1e0e26ef5350e9ad
Yookyung Kim <tituvely@gmail.com>
2022-04-03 01:35:25 +0900

    spEL(스프링 Expression Language)

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/AppRunner.java
@@ -1,20 +1,54 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.convert.ConversionService;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    @Value("${my.value}")
+    int myValue;
+
+    @Value("#{${my.value} eq 100}")
+    boolean isMyValue100;
+
+    @Value("#{'spring'}")
+    String spring;
+
+    @Value("#{sample.data}")
+    int sampleData;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService);
-        System.out.println(conversionService.getClass());
+        System.out.println("value = " + value);
+        System.out.println("greeting = " + greeting);
+        System.out.println("trueOrFalse = " + trueOrFalse);
+        System.out.println("hello = " + hello);
+        System.out.println("myValue = " + myValue);
+        System.out.println("isMyValue100 = " + isMyValue100);
+        System.out.println("spring = " + spring);
+        System.out.println("sampleData = " + sampleData);
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("2 + 100");
+        Integer value = expression.getValue(Integer.class);
+        System.out.println(value);
     }
 }
