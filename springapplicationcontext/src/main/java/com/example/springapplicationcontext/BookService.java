3ed1c43eb3dfc6c79e28b2e36fdc91a0cc3acdd4
Yookyung Kim <tituvely@gmail.com>
2022-04-02 17:37:21 +0900

    Spring 빈 설정 - 동일 클래스의 빈이 두개 이상일때 @Qualifier로 명시

--- springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
+++ springapplicationcontext/src/main/java/com/example/springapplicationcontext/BookService.java
@@ -1,12 +1,14 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
     @Autowired
+    @Qualifier("tituBookRepository")
     BookRepository bookRepository;
 
     public void printBookRepository() {
