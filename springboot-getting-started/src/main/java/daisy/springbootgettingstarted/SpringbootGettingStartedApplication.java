4e660b48fa5f795abd52b28badd9c9122da5f32d
Yookyung Kim <tituvely@gmail.com>
2022-04-04 00:49:35 +0900

    http2 설정

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
@@ -1,11 +1,7 @@
 package daisy.springbootgettingstarted;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +18,4 @@ public class SpringbootGettingStartedApplication {
         SpringApplication.run(SpringbootGettingStartedApplication.class, args);
     }
 
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
-    }
 }
