0e74356806e7bbf7478d85f010e6add5ff01fb8a
Yookyung Kim <tituvely@gmail.com>
2022-04-03 15:10:32 +0900

    Data 설정

--- jwt-tutorial/src/main/java/com/example/jwttutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.example.jwttutorial.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,6 +29,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/hello").permitAll()
                 .anyRequest().authenticated();
     }
-
 
 }
