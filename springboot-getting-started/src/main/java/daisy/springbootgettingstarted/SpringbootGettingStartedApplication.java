c2e3a50954d8dcc83cd34833ec447369f45b6c0c
Yookyung Kim <tituvely@gmail.com>
2022-04-03 20:49:33 +0900

    http도 받을 수 있는 Connector 추가 - curl -I -k --http2 http://localhost:8080/hello - curl -I -k --http2 https://localhost:8443/hello

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
@@ -1,7 +1,11 @@
 package daisy.springbootgettingstarted;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +18,20 @@ public class SpringbootGettingStartedApplication {
         return "hello";
     }
 
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGettingStartedApplication.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
 }
