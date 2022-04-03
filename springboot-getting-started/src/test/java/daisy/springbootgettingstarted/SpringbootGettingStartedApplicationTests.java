4d7910b73fe79c60c62e62eaea1513287a4a055f
Yookyung Kim <tituvely@gmail.com>
2022-04-04 01:40:19 +0900

    프로퍼티 우선 순위 - 테스트는 test resource 사용

--- springboot-getting-started/src/test/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplicationTests.java
+++ springboot-getting-started/src/test/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplicationTests.java
@@ -1,13 +1,25 @@
 package daisy.springbootgettingstarted;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootGettingStartedApplicationTests {
 
+    @Autowired
+    Environment environment;
+
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("daisy.name"))
+                .isEqualTo("daisy");
     }
 
 }
