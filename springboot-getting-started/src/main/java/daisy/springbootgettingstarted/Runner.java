89883830d13446841c5cb17599bd0488cc3fda19
Yookyung Kim <tituvely@gmail.com>
2022-04-04 01:28:59 +0900

    ApplicationRunner

---
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/Runner.java
@@ -0,0 +1,15 @@
+package daisy.springbootgettingstarted;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("foo: " + args.containsOption("foo"));
+        System.out.println("bar: " + args.containsOption("bar"));
+    }
+}
--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootGettingStartedApplication {
 
     public static void main(String[] args) {
-        // 따라서 명시적으로, Listener를 등록해주어야 함
         SpringApplication app = new SpringApplication(SpringbootGettingStartedApplication.class);
         app.run(args);
     }
