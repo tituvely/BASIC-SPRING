b653856271feb5b79b79b6eddb02a0d347e5de49
Yookyung Kim <tituvely@gmail.com>
2022-04-04 22:48:54 +0900

    스프링부트 Logger 설정

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SampleRunner.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SampleRunner.java
@@ -1,5 +1,7 @@
 package daisy.springbootgettingstarted;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
 
     @Autowired
     DaisyProperties daisyProperties;
@@ -16,10 +20,10 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("=============");
-        System.out.println("hello = " + hello);
-        System.out.println("name = " + daisyProperties.getName());
-        System.out.println("fullname = " + daisyProperties.getFullName());
-        System.out.println("=============");
+        logger.info("=============");
+        logger.info("hello = " + hello);
+        logger.info("name = " + daisyProperties.getName());
+        logger.info("fullname = " + daisyProperties.getFullName());
+        logger.info("=============");
     }
 }
