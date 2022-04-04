b755f08bb61ed7977c1d9ab8b35bc053de6fb1d7
Yookyung Kim <tituvely@gmail.com>
2022-04-04 23:12:13 +0900

    통합 Test - Mocking

--- springboot-getting-started/src/test/java/daisy/springbootgettingstarted/SampleControllerTest.java
+++ springboot-getting-started/src/test/java/daisy/springbootgettingstarted/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,10 +18,15 @@ class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("titu");
+
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello daisy");
+        assertThat(result).isEqualTo("hello titu");
     }
 
 }
