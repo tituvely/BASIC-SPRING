3395d93be284f2d2f53eef8040143f3ccca58171
Yookyung Kim <tituvely@gmail.com>
2022-04-07 10:07:48 +0900

    스프링 웹 MVC - HttpMessageConverters

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/user/UserController.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/user/UserController.java
@@ -1,6 +1,8 @@
 package daisy.springbootgettingstarted.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) {
+        return user;
     }
 
 }
