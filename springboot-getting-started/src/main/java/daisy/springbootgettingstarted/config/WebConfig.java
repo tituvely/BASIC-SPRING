4364c126354b199c9dcaa4bb85ed69a0c20d58e6
Yookyung Kim <tituvely@gmail.com>
2022-04-07 09:50:18 +0900

    스프링 웹 MVC 설정 확장

---
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/config/WebConfig.java
@@ -0,0 +1,10 @@
+package daisy.springbootgettingstarted.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
