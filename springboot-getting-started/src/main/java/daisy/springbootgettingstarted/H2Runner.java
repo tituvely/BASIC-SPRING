4cf48788b489c07ce7896c4d262643b2bdc54d4b
Yookyung Kim <tituvely@gmail.com>
2022-04-10 00:41:59 +0900

    스프링 데이터 - 인메모리 데이터베이스(h2)
    - spring.h2.console.enabled=true로 /h2-console 이용 가능

---
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/H2Runner.java
@@ -0,0 +1,35 @@
+package daisy.springbootgettingstarted;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Connection connection = dataSource.getConnection();
+        System.out.println("url: " + connection.getMetaData().getURL());
+        System.out.println("username: " + connection.getMetaData().getUserName());
+        Statement statement = connection.createStatement();
+        String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+        statement.executeUpdate(sql);
+        statement.close();
+        connection.close();
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'daisy')");
+    }
+}
--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
@@ -2,19 +2,9 @@ package daisy.springbootgettingstarted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootGettingStartedApplication {
-
-    @CrossOrigin(origins = "http://localhost:18080")
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringbootGettingStartedApplication.class);
