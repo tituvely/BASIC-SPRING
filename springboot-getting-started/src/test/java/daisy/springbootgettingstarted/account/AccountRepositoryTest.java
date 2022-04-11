cd44ee1fd675e5cbde510a98f829d4e9df8595c4
Yookyung Kim <tituvely@gmail.com>
2022-04-11 19:40:36 +0900

    스프링 데이터 - 스프링 데이터 JPA 테스트

--- springboot-getting-started/src/test/java/daisy/springbootgettingstarted/account/AccountRepositoryTest.java
+++ springboot-getting-started/src/test/java/daisy/springbootgettingstarted/account/AccountRepositoryTest.java
@@ -3,17 +3,18 @@ package daisy.springbootgettingstarted.account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class AccountRepositoryTest {
 
     @Autowired
@@ -27,11 +28,17 @@ public class AccountRepositoryTest {
 
     @Test
     public void di() throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getURL());
-            System.out.println(metaData.getDriverName());
-            System.out.println(metaData.getUserName());
-        }
+        Account account = new Account();
+        account.setUsername("daisy");
+        account.setPassword("pass");
+
+        Account newAccount = accountRepository.save(account);
+        assertThat(newAccount).isNotNull();
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(account.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonExistingAccount = accountRepository.findByUsername("titu");
+        assertThat(nonExistingAccount).isEmpty();
     }
 }
