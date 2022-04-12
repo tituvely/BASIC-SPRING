c97840be238a1dd52fe2a3d234dc31474445e419
Yookyung Kim <tituvely@gmail.com>
2022-04-13 00:11:05 +0900

    스프링 시큐리티 - Starter-Security
    - 모든 요청에 인증이 필요함
    - 기본 사용자 생성 (Username: user)
    - 테스트는 @WithMockUser로

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package daisy.springbootgettingstarted.account;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-
-public interface AccountRepository extends Neo4jRepository<Account, Long> {
-}
