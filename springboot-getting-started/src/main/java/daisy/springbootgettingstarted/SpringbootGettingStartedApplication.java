553b832f2e50a7175012c75d99ed971ef1c6ee39
Yookyung Kim <tituvely@gmail.com>
2022-04-12 19:20:56 +0900

    스프링 데이터 - MongoDB(MongoTemplate 사용)

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/SpringbootGettingStartedApplication.java
@@ -1,14 +1,33 @@
 package daisy.springbootgettingstarted;
 
+import daisy.springbootgettingstarted.account.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class SpringbootGettingStartedApplication {
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringbootGettingStartedApplication.class);
         app.run(args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            Account account = new Account();
+            account.setName("daisy");
+            account.setEmail("daisy@spring.com");
+
+            mongoTemplate.insert(account);
+        };
     }
 
 }
