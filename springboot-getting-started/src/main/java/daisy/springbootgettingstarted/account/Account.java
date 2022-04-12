4d3e8c050b74d6922f2451ed6db70b174f773e60
Yookyung Kim <tituvely@gmail.com>
2022-04-12 19:10:11 +0900

    스프링 데이터 - Redis

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/account/Account.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/account/Account.java
@@ -1,41 +1,37 @@
 package daisy.springbootgettingstarted.account;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Entity
+@RedisHash("accounts")
 public class Account {
 
     @Id
-    @GeneratedValue
-    private Long id;
+    private String id;
+    private String name;
+    private String email;
 
-    private String username;
-
-    private String password;
-
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getPassword() {
-        return password;
+    public String getEmail() {
+        return email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/account/AccountRepository.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/account/AccountRepository.java
@@ -1,9 +1,6 @@
 package daisy.springbootgettingstarted.account;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByUsername(String username);
+public interface AccountRepository extends CrudRepository<Account, String> {
 }
