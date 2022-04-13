a70d101028fc93e47747c6bd1a64bd16e599740c
Yookyung Kim <tituvely@gmail.com>
2022-04-13 18:39:35 +0900

    스프링 REST 클라이언트 - WebClient (Non-Blocking, Asynchronous)

--- springboot-getting-started/src/main/java/daisy/springbootgettingstarted/RestRunner.java
+++ springboot-getting-started/src/main/java/daisy/springbootgettingstarted/RestRunner.java
@@ -3,31 +3,52 @@ package daisy.springbootgettingstarted;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RestRunner implements ApplicationRunner {
 
     @Autowired
-    RestTemplateBuilder restTemplateBuilder;
+    WebClient.Builder builder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        RestTemplate restTemplate = restTemplateBuilder.build();
+        WebClient webClient = builder.build();
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
-        System.out.println(helloResult);
+        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+                .retrieve()
+                .bodyToMono(String.class);
 
-        String myResult = restTemplate.getForObject("http://localhost:8080/my", String.class);
-        System.out.println(myResult);
+        helloMono.subscribe(s -> {
+            System.out.println(s);
 
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+
+        Mono<String> myMono = webClient.get().uri("http://localhost:8080/my")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        myMono.subscribe(s -> {
+            System.out.println(s);
+
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
     }
 }
