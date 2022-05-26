d68230c9266644a1eccc4cfcfc66e413bd9e8435
Yookyung Kim <tituvely@gmail.com>
2022-05-26 21:38:14 +0900

    [Spring Boot를 이용한 RESTful Web Services 개발] - URI를 이용한 REST API Version 관리

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,10 +1,9 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -12,6 +11,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonFilter("UserInfo")
 public class User {
     private Integer id;
