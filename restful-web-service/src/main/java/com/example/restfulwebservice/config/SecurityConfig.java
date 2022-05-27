aa45811d2b2e2e3c8d7bc04c4df945c442dc57ad
Yookyung Kim <tituvely@gmail.com>
2022-05-27 13:34:47 +0900

    [Spring Boot를 이용한 RESTful Web Services 개발] - Configuration 클래스를 이용한 사용자 인증 처리

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("daisy")
+                .password("pw")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
