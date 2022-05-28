391decd1571d1c314338acf7ddb617f21c4c7594
Yookyung Kim <tituvely@gmail.com>
2022-05-28 14:41:26 +0900

    [Spring Boot를 이용한 RESTful Web Services 개발] - JPA를 사용을 위한 Dependency 추가와 설정

--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.example.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
