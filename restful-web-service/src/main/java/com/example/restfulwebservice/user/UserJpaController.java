71a5c456234bf0d69b2a237a8e68e681f03ff63c
Yookyung Kim <tituvely@gmail.com>
2022-05-29 21:48:24 +0900

    [Spring Boot를 이용한 RESTful Web Services 개발] - JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -1,11 +1,12 @@
 package com.example.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +31,23 @@ public class UserJpaController {
         }
 
         return user.get();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
