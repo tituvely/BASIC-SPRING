5193ff5659705761dc18f67f32aa6efc2ae1daa2
Yookyung Kim <tituvely@gmail.com>
2022-05-29 21:42:27 +0900

    [Spring Boot를 이용한 RESTful Web Services 개발] - JPA를 이용한 사용자 목록 조회 - GET HTTP Method

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -2,10 +2,12 @@ package com.example.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -18,5 +20,16 @@ public class UserJpaController {
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
     }
-    
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user.get();
+    }
+
 }
