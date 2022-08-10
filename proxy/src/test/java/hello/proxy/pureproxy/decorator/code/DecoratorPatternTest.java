0a6692e4958bf288a7bd323c34abe14b083a9c51
Yookyung Kim <tituvely@gmail.com>
2022-08-10 22:22:24 +0900

    [스프링 핵심원리 고급] 프록시 패턴과 데코레이터 패턴 데코레이터패턴 적용 - 부가 기능 추가(실행 시간을 측정하는 데코레이터)

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
@@ -21,4 +21,13 @@ public class DecoratorPatternTest {
         client.execute();
     }
 
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+
 }
